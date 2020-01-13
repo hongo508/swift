b2e0284ea4f193b8c65f32d71203d427bf0fe168
hongo508 <wearneevevge@i.softbank.jp>
2020-01-14 00:12:13 +0900

    Swift Storyboard を追加

---
+++ storyboard-test/storyboard-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboard-test
+//
+//  Created by 本郷匠 on 2020/01/14.
+//  Copyright © 2020 t.hongo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
