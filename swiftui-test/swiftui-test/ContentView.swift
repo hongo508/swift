d6f94e16e6a4d02b0f629e9daa77eb7aeab3e13a
hongo508 <wearneevevge@i.softbank.jp>
2020-01-14 00:01:53 +0900

    SwiftUI デフォルトモジュール作成

---
+++ swiftui-test/swiftui-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-test
+//
+//  Created by 本郷匠 on 2020/01/13.
+//  Copyright © 2020 t.hongo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
