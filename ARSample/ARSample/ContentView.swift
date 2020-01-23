b18710fbef68333bf1987a852821e7718427d804
hongo508 <wearneevevge@i.softbank.jp>
2020-01-23 22:25:40 +0900

    ARSample作成

---
+++ ARSample/ARSample/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  ARSample
+//
+//  Created by 本郷匠 on 2020/01/23.
+//  Copyright © 2020 t.hongo. All rights reserved.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
